95203ac7b483ae812481cecc4515538ad16c8f8d
cemery1 <cemery@andrew.cmu.edu>
2018-11-30 15:47:32 -0500

    Update LearnCard.js

--- wick-site/src/LearnPage/LearnCard.js
+++ wick-site/src/LearnPage/LearnCard.js
@@ -63,7 +63,7 @@ const LearnCard = ({title, body, image, link, launchEditor, useImage, useButtonR
                     </Col>
                 </Row>
                 {useButtonRow && (
-                    <Row className="LearnCard-editorButtonRow">
+                    <Row className="zoomInDown animated LearnCard-editorButtonRow">
                         <Col xs="12">
                             <Button
                                 color="info"
